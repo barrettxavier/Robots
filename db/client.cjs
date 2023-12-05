0dae85fb20913c14f2563867b47c9673c67d005a
barrettxavier <barrettxavier@yahoo.com>
2023-12-05 10:56:18 -0500

    added const connection for database

--- db/client.cjs
+++ db/client.cjs
@@ -1,7 +1,8 @@
 /* eslint-disable no-undef */
 const { Client } = require("pg");
-const client = new Client(
-  "postgres://postgres:123456@localhost:5432/buy_robots"
-);
+const connection =
+  process.env.DATABASE_URL ||
+  "postgres://postgres:123456@localhost:5432/buy_robots";
+const client = new Client(connection);
 
 module.exports = client;
